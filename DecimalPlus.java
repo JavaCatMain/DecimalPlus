1b9ec69ea5d5a0313e0e70ce7d7fdc9982419b58
JavaCatMain <javacat.main@gmail.com>
2024-04-12 10:26:05 +1000

    DecimalPlus add/sub methods added

--- DecimalPlus.java
+++ DecimalPlus.java
@@ -1,23 +1,30 @@
+/**
+ * just very big numbers in range from -10^Double.MAX_VALUE (about -1ee308) to 10^Double.MAX_VALUE (about 1ee308)
+ */
+@SuppressWarnings("unused")
 public class DecimalPlus {
 
     double value = 0;
     private int sign = 1;
 
+    /**
+     * @param mantissa acts like multiplier
+     * @param exponent 10^exponent
+     */
     public DecimalPlus(double mantissa,double exponent){
         if(mantissa==0){
             this.sign = 0;
         }else
         if(mantissa>0){
-            this.sign = 1;
             this.value = Math.log10(mantissa)+exponent;
         }else
         {
             this.sign = -1;
-            this.value = Math.log10(mantissa)+exponent;
+            this.value = Math.log10(Math.abs(mantissa))+exponent;
         }
     }
-    public DecimalPlus(double mantissa){
-        this(mantissa,0);
+    public DecimalPlus(double basicValue){
+        this(basicValue,0);
     }
     public DecimalPlus(){
         this(0,0);
@@ -26,7 +33,14 @@ public class DecimalPlus {
     public void flipSign(){
         this.sign = -this.sign;
     }
-    
+    public void setSign(int sign) {
+        if(sign>1||sign<-1)throw new IllegalArgumentException("Sign must be 1, 0 or -1");
+        else this.sign = sign;
+    }
+
+    /**
+     * Increases current decimal value by other decimal
+     */
     public void add(DecimalPlus decimal){
         if (this.sign != 0 && decimal.sign == this.sign){
             this.value = addFormula(this.value, decimal.value);
@@ -35,6 +49,29 @@ public class DecimalPlus {
             this.value = decimal.value;
             this.sign = decimal.sign;
         }
+        else if (this.sign == -decimal.sign && this.sign != 0){
+            if(this.value == decimal.value)this.sign=0;
+            else if(this.value < decimal.value){
+                this.sign = decimal.sign;
+                this.value = subFormula(decimal.value,this.value);
+            } else {
+                this.value = subFormula(this.value,decimal.value);
+            }
+        }
+    }
+    public void add(double value){
+        add(new DecimalPlus(value));
+    }
+    /**
+     * Decreases current decimal value by other decimal
+     * (just add method with flipped sign)
+     */
+    public void sub(DecimalPlus decimal){
+        decimal.flipSign();
+        add(decimal);
+    }
+    public void sub(double decimal){
+        add(-decimal);
     }
     
     private double addFormula(double a, double b){
@@ -54,23 +91,25 @@ public class DecimalPlus {
 
     public String toString(boolean isRounded) {
         if(this.sign == 0 && isRounded)return "0";
-        if(this.sign == 0 && !isRounded)return "0.000";
+        if(this.sign == 0)return "0.000";
 
-        String out = "";
+        String out;
         double mantissa = Math.floor(Math.pow(10,(this.value % 1))*100)/100;;
         double exponent = Math.floor(this.value);
 
         if(this.value>3 || (this.value<-3 && !isRounded)){
-            out = mantissa + "e" + (exponent+"").substring(0,(int)Math.floor(Math.log10(exponent)+1));
+            out = mantissa + "e" + (exponent+"");//.substring(0,(int)Math.floor(Math.log10(exponent)+1));
         }
         else {
             if (isRounded) {
-                out = (long)Math.round(Math.pow(10, this.value)) + "";
+                out = Math.round(Math.pow(10, this.value)) + "";
             } else {
-                out = ((Math.pow(10, this.value+1)/10) + "").substring(0, 5);
+                out = ((Math.pow(10, this.value+1)/10) + "");
+                out = out.substring(0,Math.min(out.length(),5));
+                out = out + "0".repeat(5-out.length());
             }
         }
-        if(this.sign==-1){out = "-"+out;}
+        if(this.sign==-1 && !out.equals("0")){out = "-"+out;}
         return out;
     }
     public String toString(){
---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        DecimalPlus sDF = new DecimalPlus(1,Double.MAX_VALUE);
+        System.out.print(sDF);
+    }
+}
